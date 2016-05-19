a3902c9f6058b9b703b8f370d8d8f4825a531e57
Shuai Lin <linshuai2012@gmail.com>
2016-05-19 12:04:01 +0800

    Replace tabs with spaces.

--- lib/searpc-utils.c
+++ lib/searpc-utils.c
@@ -247,52 +247,52 @@ GObject *json_gobject_deserialize (GType gtype, json_t *object)
 ssize_t
 pipe_write_n(int fd, const void *vptr, size_t n)
 {
-	size_t		nleft;
-	ssize_t		nwritten;
-	const char	*ptr;
+    size_t      nleft;
+    ssize_t     nwritten;
+    const char  *ptr;
 
-	ptr = vptr;
-	nleft = n;
-	while (nleft > 0) {
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
 #ifndef WIN32
         if ( (nwritten = write(fd, ptr, nleft)) <= 0)
 #else
         if ( (nwritten = send(fd, ptr, nleft, 0)) <= 0)
 #endif
         {
-			if (nwritten < 0 && errno == EINTR)
-				nwritten = 0;		/* and call write() again */
-			else
-				return(-1);			/* error */
-		}
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0;       /* and call write() again */
+            else
+                return(-1);         /* error */
+        }
 
-		nleft -= nwritten;
-		ptr   += nwritten;
-	}
-	return(n);
+        nleft -= nwritten;
+        ptr   += nwritten;
+    }
+    return(n);
 }
 
 // Read "n" bytes from a descriptor.
 ssize_t
 pipe_read_n(int fd, void *vptr, size_t n)
 {
-	size_t	nleft;
-	ssize_t	nread;
-	char	*ptr;
+    size_t  nleft;
+    ssize_t nread;
+    char    *ptr;
 
-	ptr = vptr;
-	nleft = n;
-	while (nleft > 0) {
-		if ( (nread = read(fd, ptr, nleft)) < 0) {
-			if (errno == EINTR)
-				nread = 0;		/* and call read() again */
-			else
-				return(-1);
-		} else if (nread == 0)
-			break;				/* EOF */
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if ( (nread = read(fd, ptr, nleft)) < 0) {
+            if (errno == EINTR)
+                nread = 0;      /* and call read() again */
+            else
+                return(-1);
+        } else if (nread == 0)
+            break;              /* EOF */
 
-		nleft -= nread;
-		ptr   += nread;
-	}
-	return(n - nleft);		/* return >= 0 */
+        nleft -= nread;
+        ptr   += nread;
+    }
+    return(n - nleft);      /* return >= 0 */
 }
