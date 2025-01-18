4c5a914fdd5f8c093e5025056114780b4463cc1d
杨赫然 <heran.yang@seafile.com>
2025-01-18 13:49:54 +0800

    Set slow threshold when log to stdout

--- lib/searpc-server.c
+++ lib/searpc-server.c
@@ -210,6 +210,10 @@ searpc_server_init_with_slow_log (RegisterMarshalFunc register_func,
     if (g_strcmp0(log_to_stdout_env, "true") == 0) {
         slow_log_fp = stdout;
         log_to_stdout = TRUE;
+        slow_threshold = slow_threshold_in;
+        filtered_funcs = filtered_funcs_in;
+
+        pthread_mutex_init (&slow_log_lock, NULL);
     } else if (slow_log_path) {
         slow_log_fp = fopen (slow_log_path, "a+");
         if (!slow_log_fp) {
